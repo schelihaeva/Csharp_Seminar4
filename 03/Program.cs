8fd38cdbad8f62a49c09dc99143eede26878a864
shelihaeva <smelkovagolodnova@yandex.ru>
2023-10-21 12:34:05 +0400

    задача на семинаре переворот значений

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,2 +1,41 @@
 ﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+using System.Reflection.Metadata.Ecma335;
+
+int[] FillArrayRnd()
+{
+    int arrSizeRnd = new Random().Next(2, 11); //задаем размер массива от 2 до 20
+    int[] retVal = new int[arrSizeRnd];
+    Random rndGenVal = new Random(); // задаем ячейку памяти для массива
+    for (int i = 0; i < arrSizeRnd; i++) // задаем цикл
+        retVal[i] = (int)rndGenVal.Next(-9, 10); // возвращаем целое число
+    
+    return retVal; // возвращаем функцию
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i]);
+        Console.Write(",");
+
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+void ArrayReplaceNegPos(int[] array){
+    
+    for(int i=0; i<array.Length; i++)
+        array[i]=-array[i]; // переворачиваем значение
+ }
+
+
+int [] myArray = FillArrayRnd();
+PrintArray(myArray);
+ArrayReplaceNegPos(myArray);
+Console.WriteLine();
+PrintArray(myArray);
