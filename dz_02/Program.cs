82778b216643230da9facb0766e9f39f93b736ca
shelihaeva <smelkovagolodnova@yandex.ru>
2023-10-23 11:04:33 +0400

    сумма нечетных индексов

---
+++ dz_02/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая находит сумму элементов с нечетными индексами в одномерном массиве целых чисел и выводит результат на экран.
+
+// Программа должна иметь метод SumOddElements, который принимает массив целых чисел и возвращает сумму элементов с нечетными индексами в массиве.
+
+// Программа должна иметь метод PrintArray, который выводит элементы массива на экран.
+
+// При выводе элементы массива должны быть разделены символом табуляции "\t".
+
+// Аргументы, передаваемые в метод/функцию:
+
+
+// '18, 76, 11'
+// На выходе:
+
+
+// 18      76      11  
+// Сумма нечетных элементов: 76
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Наш массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
